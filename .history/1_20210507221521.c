dbab873dc2f749b719f1163e2260aff23c6ca61b
Radik0 <h18170328912@gmail.com>
2021-05-07 23:59:17 +0800

    master

---
+++ .history/1_20210507221521.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 类型
+#define 有 int
+#define 云 char
+#define 空 void
+#define 皆 const
+
+// 关键字
+#define 究竟 return
+#define 如是我闻， typedef
+#define 佛 struct
+#define 量 sizeof
+#define 若 for
+
+// 符号
+#define 非 (
+#define 何 )
+#define 哉 )
+#define 无 [
+#define 而 ].
+#define 者 ]
+#define ， ,
+#define ； ;
+#define 也 ;
+#define 曰 {
+#define ｡ }
+#define 于 *
+#define 法 <
+#define 是 =
+#define 当 ++
+#define ？ -
+#define 不 .
+#define 惟 &
+#define 又 ->
+
+// 常量
+#define 涅槃 0
+#define 相 10
+#define 挂碍 32
+#define 自在 "%s"
+#define 解脱 "%d"
+#define 境地 "\n"
+
+// 标识符
+#define 一切众生 main
+#define 转世 scanf
+#define 放生 printf
+#define 功德 qsort
+#define 神通 bsearch
+
+如是我闻， 佛 曰 云 生 无 挂碍 者 ； 有 灭 也 ｡
+
+般若波罗蜜多 ； 般若波罗蜜多 舍利子 无 相 者 ；
+有 五蕴 非 皆 空 于 我 ， 皆 空 于 物 哉 曰 究竟 非 非 般若波罗蜜多 于 何 物 哉 又 灭 ？
+非 非 般若波罗蜜多 于 何 我 哉 又 灭 也 ｡
+
+有 一切众生 非 哉 曰 有 为 是 涅槃 ；
+般若波罗蜜多 佛法 ； 若 非 ； 为 法 相 ；
+当 为 何 转世 非 自在 解脱 ， 舍利子 无 为 而 生 ， 惟 舍利子 无 为 而 灭 哉 ；
+功德 非 舍利子 ， 相 ， 量 非 般若波罗蜜多 哉 ， 五蕴 哉 ；
+转世 非 解脱 ， 惟 佛法 不 灭 哉 ；
+放生 非 自在 境地 ， 非 非 般若波罗蜜多 于 何 神通 非 惟 佛法 ， 舍利子 ， 相 ， 量 非 般若波罗蜜多 哉 ， 五蕴 何 哉 又 生 哉 ；
+究竟 涅槃 也 ｡
